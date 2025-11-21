3dcb271a539398825c1d677c0e0f5c7ef261c64e
asalas-smarteam <asalas@smarteamcr.com>
2025-11-21 15:31:05 -0600

    Add configuration controller and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 /* Register all routes */
 import echoRoutes from './echo.routes.js';
 import healthRoutes from './health.routes.js';
+import configRoutes from './config.routes.js';
 
 export default async function routes(app) {
   app.register(echoRoutes, { prefix: '/echo_test' });
   app.register(healthRoutes);
+  app.register(configRoutes);
 }
