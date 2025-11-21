c50d67960619a9f7873ea1466d51daa232d19d21
asalas-smarteam <asalas@smarteamcr.com>
2025-11-21 16:27:29 -0600

    Add mapping controller and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@
 import echoRoutes from './echo.routes.js';
 import healthRoutes from './health.routes.js';
 import configRoutes from './config.routes.js';
+import mappingRoutes from './mapping.routes.js';
 
 export default async function routes(app) {
   app.register(echoRoutes, { prefix: '/echo_test' });
   app.register(healthRoutes);
   app.register(configRoutes);
+  app.register(mappingRoutes);
 }
