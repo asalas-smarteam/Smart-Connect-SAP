5ca897994d63f20673589bea283a2c12ea34506d
asalas-smarteam <asalas@smarteamcr.com>
2025-11-21 22:36:04 -0600

    Add SyncLog model

---
+++ src/db/models/SyncLog.js
@@ -0,0 +1,33 @@
+export default function SyncLog({ sequelize }, DataTypes) {
+  return sequelize.define(
+    'SyncLog',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      clientConfigId: {
+        type: DataTypes.INTEGER,
+      },
+      status: {
+        type: DataTypes.STRING,
+      },
+      recordsProcessed: {
+        type: DataTypes.INTEGER,
+      },
+      errorMessage: {
+        type: DataTypes.TEXT,
+      },
+      startedAt: {
+        type: DataTypes.DATE,
+      },
+      finishedAt: {
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+}
