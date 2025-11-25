87ce004a70d2f1b37b33584b234118651f548d66
asalas-smarteam <asalas@smarteamcr.com>
2025-11-25 15:07:47 -0600

    Update mapping service to use hubspot credentials

--- src/services/mapping.service.js
+++ src/services/mapping.service.js
@@ -1,8 +1,30 @@
-import { FieldMapping, ClientConfig } from '../config/database.js';
+import { FieldMapping, ClientConfig, HubspotCredentials } from '../config/database.js';
 
 const mappingService = {
   async getMappings(clientConfigId, objectType) {
     try {
+      if (clientConfigId) {
+        const config = await ClientConfig.findByPk(clientConfigId);
+
+        if (config) {
+          const credentials = await HubspotCredentials.findOne({
+            where: { clientConfigId: config.id },
+          });
+
+          if (!credentials) {
+            return [];
+          }
+
+          return await FieldMapping.findAll({
+            where: {
+              hubspotCredentialId: credentials.id,
+              objectType,
+            },
+            order: [['id', 'ASC']],
+          });
+        }
+      }
+
       return await FieldMapping.findAll({
         where: { clientConfigId, objectType },
         order: [['id', 'ASC']],
