04cfb30276f2b85f9138fce4701d0c279bedf587
asalas-smarteam <asalas@smarteamcr.com>
2026-01-18 17:06:12 -0600

    Add tenant OAuth init endpoint

--- src/routes/oauth.routes.js
+++ src/routes/oauth.routes.js
@@ -1,7 +1,8 @@
-import { initOAuth, oauthCallback } from '../controllers/oauth.controller.js';
+import { initOAuth, initOAuthForTenant, oauthCallback } from '../controllers/oauth.controller.js';
 import { tenantResolver } from '../middleware/tenantResolver.js';
 
 export default async function routes(app) {
   app.get('/oauth/hubspot/init/:clientConfigId', { preHandler: tenantResolver }, initOAuth);
   app.get('/oauth/hubspot/callback', { preHandler: tenantResolver }, oauthCallback);
+  app.post('/oauth/hubspot/init', initOAuthForTenant);
 }
