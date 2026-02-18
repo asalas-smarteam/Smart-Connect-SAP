d6b477f351e77f99f528f10cbeb5a475f4b0f28e
Alejandro <asalas@smarteamcr.com>
2026-02-18 13:27:07 -0600

    last changes in mapping service

--- src/services/mapping.service.js
+++ src/services/mapping.service.js
@@ -1,80 +1,3 @@
-<<<<<<< HEAD
-function getTenantFieldMapping(tenantModels) {
-  if (!tenantModels) {
-    throw new Error('Tenant models are required for mapping operations');
-  }
-
-  return tenantModels.FieldMapping;
-}
-
-const mapFields = (inputData, mappings, objectType) => {
-  const result = {};
-
-  mappings
-    .filter((mapping) => mapping.isActive ?? true)
-    .forEach((m) => {
-      result[m.targetField] = inputData[m.sourceField] || null;
-    });
-
-  const mappedFields = { properties: result };
-
-  if (objectType === 'deal') {
-    const specialDealFields = [
-      'associatedContacts',
-      'associatedCompanies',
-      'associatedProducts',
-    ];
-
-    specialDealFields.forEach((field) => {
-      if (Object.prototype.hasOwnProperty.call(inputData, field)) {
-        mappedFields[field] = inputData[field];
-      }
-    });
-  }
-
-  return mappedFields;
-};
-
-const mappingService = {
-  async getMappings(hubspotCredentialId, objectType, tenantModels) {
-    try {
-      if (!hubspotCredentialId) {
-        return [];
-      }
-
-      const FieldMapping = getTenantFieldMapping(tenantModels);
-      return await FieldMapping.find({ hubspotCredentialId, objectType }).sort({ _id: 1 });
-    } catch (error) {
-      console.error('Failed to fetch mappings:', error);
-      return [];
-    }
-  },
-
-  async mapRecords(sapRecords, hubspotCredentialId, objectType, tenantModels) {
-    try {
-      const mappings = await this.getMappings(hubspotCredentialId, objectType, tenantModels);
-
-      return sapRecords.map((record) => mapFields(record, mappings, objectType));
-    } catch (error) {
-      console.error('Failed to apply mappings:', error);
-      return [];
-    }
-  },
-
-  async applyMapping(inputData, hubspotCredentialId, objectType, tenantModels) {
-    try {
-      const mappings = await this.getMappings(hubspotCredentialId, objectType, tenantModels);
-
-      return mapFields(inputData, mappings, objectType);
-    } catch (error) {
-      console.error('Failed to apply mappings:', error);
-      return {};
-    }
-  },
-};
-
-export default mappingService;
-=======
 function getTenantFieldMapping(tenantModels) {
   if (!tenantModels) {
     throw new Error('Tenant models are required for mapping operations');
@@ -212,4 +135,4 @@ const mappingService = {
 };
 
 export default mappingService;
->>>>>>> 8944011596f9c4089260378e2821ec11348706c1
+
