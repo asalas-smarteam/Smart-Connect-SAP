be8183a48578dc86eec3da145bc981db1630fd81
asalas-smarteam <asalas@smarteamcr.com>
2026-02-27 14:44:56 -0600

    feat(master): add DefaultSapFilter model factory

---
+++ models/master/defaultSapFilter.model.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+export const defaultSapFilterSchema = new Schema(
+  {
+    objectType: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    property: {
+      type: String,
+      required: true,
+    },
+    operator: {
+      type: String,
+      required: true,
+      enum: ['eq', 'ge'],
+    },
+    value: {
+      type: String,
+      default: null,
+    },
+    isDefault: {
+      type: Boolean,
+      default: true,
+    },
+    isDynamic: {
+      type: Boolean,
+      default: false,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    collection: 'DefaultSapFilters',
+    timestamps: true,
+  }
+);
+
+export function createDefaultSapFilterModel(connection) {
+  return connection.models.DefaultSapFilter || connection.model('DefaultSapFilter', defaultSapFilterSchema);
+}
