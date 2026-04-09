bfddfb2afff93a96e928d1fd44a9c14ae404f9fe
Alejandro <asalas@smarteamcr.com>
2026-04-09 13:55:50 -0600

    changes info in echo test

--- src/controllers/echo.controller.js
+++ src/controllers/echo.controller.js
@@ -3,15 +3,58 @@ import { requireTenantModels } from '../utils/tenantModels.js';
 
 export const echoTest = async (req, reply) => {
   const { body } = req;
-  const { LogEntry } = requireTenantModels(req);
 
   logger.info('Echo test received', body);
 
-  await LogEntry.create({
-    type: 'ECHO_TEST',
-    payload: body,
-    level: 'info',
-  });
-
   return reply.send({ ok: true, body });
 };
+/*
+{
+  eventId: 797713315,
+  subscriptionId: 6174090,
+  portalId: 50564010,
+  appId: 31481725,
+  occurredAt: 1775764313528,
+  subscriptionType: "deal.associationChange",
+  attemptNumber: 0,
+  changeSource: "USER",
+  associationType: "DEAL_TO_LINE_ITEM",
+  fromObjectId: 58986911596,
+  toObjectId: 54154480712,
+  associationRemoved: false,
+  isPrimaryAssociation: false,
+}
+
+
+{
+  eventId: 797713315,
+  subscriptionId: 6174090,
+  portalId: 50564010,
+  appId: 31481725,
+  occurredAt: 1775764313528,
+  subscriptionType: "deal.associationChange",
+  attemptNumber: 1,
+  changeSource: "USER",
+  associationType: "DEAL_TO_LINE_ITEM",
+  fromObjectId: 58986911596,
+  toObjectId: 54154480712,
+  associationRemoved: false,
+  isPrimaryAssociation: false,
+}
+
+{
+  eventId: 797713315,
+  subscriptionId: 6174090,
+  portalId: 50564010,
+  appId: 31481725,
+  occurredAt: 1775764313528,
+  subscriptionType: "deal.associationChange",
+  attemptNumber: 2,
+  changeSource: "USER",
+  associationType: "DEAL_TO_LINE_ITEM",
+  fromObjectId: 58986911596,
+  toObjectId: 54154480712,
+  associationRemoved: false,
+  isPrimaryAssociation: false,
+}
+*/
--- src/routes/echo.routes.js
+++ src/routes/echo.routes.js
@@ -2,5 +2,5 @@ import { echoTest } from '../controllers/echo.controller.js';
 import { tenantResolver } from '../middleware/tenantResolver.js';
 
 export default async function routes(app) {
-    app.post('/', { preHandler: tenantResolver }, echoTest);
+    app.post('/', echoTest);
 }
